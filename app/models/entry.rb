88be7dabbaa016ee0af85a39d9bf9d86912e0e20
Anthony M <anthony@followapp.me>
2018-11-12 18:59:17 -0500

    Fix entry by date query to use appropriate time zone offset

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,14 +4,13 @@ class Entry < ApplicationRecord
   validates :body, presence: true
   validates :user, presence: true
 
-  def self.grouped_by_date(date_array)
-    entries_hash = where("date(created_at::date AT TIME ZONE '#{Time.zone.name}') IN (?)", date_array).
-        group_by { |entry| entry.created_at.to_date.to_s}
+  def self.created_on(dates)
+    where("date(created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{Time.zone.name}') IN (?)", dates)
+  end
 
-    entries_hash.each do |date, entry|
-      entries_hash[date] = entry.first if entry.present?
-    end
-
+  def self.grouped_by_date(dates)
+    entries_hash = created_on(dates).group_by { |entry| entry.created_at.to_date.to_s }
+    entries_hash.each { |date, entry| entries_hash[date] = entry&.first }
     entries_hash
   end
 end
